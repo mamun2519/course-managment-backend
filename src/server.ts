25fd2ef91688818eb23598d39fb32c71f6b649b3
Juboraj Islam Mamun <juborajvai22@gmail.com>
2023-12-02 12:12:20 +0600

    add to config file

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,8 @@
 import mongoose from "mongoose";
 import app from "./app";
-import config from "./config";
+
 import { Server } from "http";
+import config from "./config";
 
 let server: Server;
 // Uncaught Exception Error Handler
